dd0f05c41bc6aa1b01597c2731f6f9c44c15d133
jihye-94 <sn022317@gmail.com>
2021-09-13 18:17:23 +0900

    20210913

---
+++ src/main/java/kr/or/bit/service/MemoAddService.java
@@ -0,0 +1,51 @@
+package kr.or.bit.service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import kr.or.bit.action.Action;
+import kr.or.bit.action.ActionForward;
+import kr.or.bit.dao.memodao;
+import kr.or.bit.dto.memo;
+
+public class MemoAddService implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+		
+    	String id = request.getParameter("id");
+    	String email = request.getParameter("email");
+    	String content = request.getParameter("memo");
+    	
+    	//memo m = new memo();  //권장사항
+    	//m.setId(id);
+    	
+    	memodao dao = new memodao(); //POINT
+		int result = dao.insertMemo(id, email, content);
+    	
+		 	String msg="";
+		 	String url="";
+		    if(result > 0){
+		    	msg ="등록성공";
+		    	url ="MemoList.memo";
+		    }else{
+		    	msg="등록실패";
+		    	url="memo.html";
+		    }
+		    
+		    request.setAttribute("board_msg",msg);
+		    request.setAttribute("board_url", url);
+		
+		    ActionForward forward = new ActionForward();
+		    forward.setRedirect(false);
+		    forward.setPath("/WEB-INF/views/redirect.jsp");
+		    
+		return forward;
+	}
+
+}
+
+
+
+
+
+
